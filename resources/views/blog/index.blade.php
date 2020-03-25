e8a591478eb57c10edcf159f5ce87cf4b3c1ff6c
dzintars-magons <dzintars.magons@gmail.com>
2020-03-25 21:33:56 +0200

    added some bootstrap classes to make better look

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,15 +3,17 @@
 @section('content')
 
 <div class="container">
-    <ul>
+    <ul class="list-unstyled">
         @foreach ($posts as $p)
-            <li>{{ $p->title }}</li>
-            <li>{{ $p->body }}</li>
-
-            @if(Auth::check())
-            <a href="{{ url('edit-post') }}/{{ $p->id }}" class="btn btn-warning">Edit</a>
-            <a href="{{ url('delete-post') }}/{{ $p->id }}" class="btn btn-outline-danger">Delete</a>
-            @endif
+            <li><h5>{{ $p->title }}</h5></li>
+            <li class="mb-5">{{ $p->body }}</li>
+            
+                @if(Auth::check())
+                <a href="{{ url('edit-post') }}/{{ $p->id }}" class="btn btn-warning">Edit</a>
+                <a href="{{ url('delete-post') }}/{{ $p->id }}" class="btn btn-outline-danger">Delete</a>
+                @endif
+            
+            
 
             <hr>
         @endforeach
